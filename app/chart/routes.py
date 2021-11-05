2a3041ad84e9b14f5aea76e51b6f0a96c0c84610
Mark COMAS <mc@stbedes.catholic.edu.au>
2021-11-05 13:57:38 +1100

    Charts complete

--- app/chart/routes.py
+++ app/chart/routes.py
@@ -6,9 +6,11 @@ import pandas as pd
 import plotly.express as px
 import plotly
 
+from sqlalchemy import func
+
 from app import db
 from app.chart import bp
-from app.models import Book
+from app.models import Book, Genre
 
 @bp.route('/')
 @login_required
@@ -48,6 +50,68 @@ def user_books_chart():
     # Returns the template, including the JSON data for the chart
     return render_template('chart_page.html', title = 'Books owned per user', chart_JSON = chart_JSON)
 
-# TODO: Add a route for a bar chart that compares books read per year by user
+@bp.route('/books_read')
+@login_required
+def books_read_chart():
+    # Run query to get books read per year by user and load into DataFrame
+    query = (
+        "SELECT username, books_read_per_year "
+        "FROM user u "
+    )
+    df = pd.read_sql(query, db.session.bind)
 
-# TODO: Add a route for a bar chart that compares number of books per genre
+    # Draw the chart and dump it into JSON format
+    chart = px.bar(
+        df, x ='username', y='books_read_per_year', title='Books read per year by user',
+        labels = {'username': 'Username','books_read_per_year': 'Books read per year'},
+        template = 'plotly_white'
+    )
+    chart_JSON = json.dumps(chart, cls=plotly.utils.PlotlyJSONEncoder, indent=4)
+
+    # Returns the template, including the JSON data for the chart
+    return render_template('chart_page.html', title = 'Books read per year', chart_JSON = chart_JSON)
+
+@bp.route('/books_per_genre_SQL_statement')
+@login_required
+def books_per_genre_SQL_statement_chart():
+    # Run query to get count of books per genre
+    query = (
+        "SELECT name, count(*) as number_of_books "
+        "FROM book b "
+        "JOIN genre g on b.genre_id = g.id "
+        "GROUP BY name "
+    )
+    df = pd.read_sql(query, db.session.bind)
+
+    # Draw the chart and dump it into JSON format
+    chart = px.bar(
+        df, x ='name', y='number_of_books', title='Number of books per genre',
+        labels = {'name': 'Genre','number_of_books': 'Number of books'},
+        template = 'plotly_white'
+    )
+    chart_JSON = json.dumps(chart, cls=plotly.utils.PlotlyJSONEncoder, indent=4)
+
+    # Returns the template, including the JSON data for the chart
+    return render_template('chart_page.html', title = 'Books per genre', chart_JSON = chart_JSON)
+
+@bp.route('/books_per_genre_SQLAlchemy')
+@login_required
+def books_per_genre_SQLAlchemy_chart():
+    # Run SQLAlchemy query to get count of books per genre
+    query = db.session.query(
+        Genre.name, 
+        func.count(Genre.name)
+        ).join(Book).group_by(Genre.name)
+ 
+    df = pd.read_sql(query.statement, query.session.bind)
+
+    # Draw the chart and dump it into JSON format
+    chart = px.bar(df, x ='name', y='count_1',
+        labels = {'name': 'Genre','count_1': 'Number of books'},
+        title='Number of books per genre',
+        template='plotly_white')
+    
+    chart_JSON = json.dumps(chart, cls=plotly.utils.PlotlyJSONEncoder, indent=4)
+
+    # Returns the template, including the JSON data for the chart
+    return render_template('chart_page.html', title = 'Books per genre', chart_JSON = chart_JSON)
